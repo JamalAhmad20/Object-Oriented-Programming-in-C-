6d10d78dd88bf076161a0c1b88d1122f8d3f2f83
Nargis <nargistahara@gmail.com>
2020-04-18 11:39:47 +0200

    for loop

---
+++ forLoops/cmake-build-debug/CMakeFiles/forLoops.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/forLoops.dir/main.cpp.o"
+  "forLoops"
+  "forLoops.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/forLoops.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ forLoops/cmake-build-debug/CMakeFiles/forLoops.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for forLoops.
+# This may be replaced when dependencies are built.
